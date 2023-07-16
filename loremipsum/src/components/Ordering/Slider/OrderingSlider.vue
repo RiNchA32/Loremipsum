d35f96e24cb227baa9b93d013d961dbc8d92caf6
Sam <rinat122695@gmailcom>
2023-07-16 11:39:29 +0300

    доработка слайдера

--- loremipsum/src/components/Ordering/Slider/OrderingSlider.vue
+++ loremipsum/src/components/Ordering/Slider/OrderingSlider.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="ordering-slider">
         <span class="ordering-slider__title">Sed ut perspiciatis, unde omnis iste natus</span>
-        <span class="ordering-slider__value">75 %</span>
+        <span class="ordering-slider__value">{{ sliderValue }}%</span>
         <label for="slider"
                class="ordering-slider__label"
         >
             <input id="slider"
+                   v-model="sliderValue"
                    type="range"
                    min="0"
                    max="100"
-                   value="75"
                    class="ordering-slider__input"
             >
         </label>
@@ -17,10 +17,17 @@
 </template>
 
 <script lang="ts">
-    import {defineComponent} from 'vue';
+    import {defineComponent, ref} from 'vue';
 
     export default defineComponent({
         name: 'OrderingSlider',
+        setup() {
+            const sliderValue = ref('75');
+
+            return {
+                sliderValue,
+            }
+        },
     });
 </script>
 
